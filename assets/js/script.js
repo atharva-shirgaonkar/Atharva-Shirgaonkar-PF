dc5403b652c12448a16cb8882cad88a67953bc44
Atharva Shirgaonkar <154105625+atharva-shirgaonkar@users.noreply.github.com>
2025-05-14 15:22:58 +0530

    Updated Script
    zdsf

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,23 +137,19 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 // page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
+const navLinks = document.querySelectorAll('[data-nav-link]');
+const pages = document.querySelectorAll('[data-page]');
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+// Add click event listeners to navigation buttons
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    // Remove the 'active' class from all buttons and sections
+    navLinks.forEach((btn) => btn.classList.remove('active'));
+    pages.forEach((page) => page.classList.remove('active'));
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-
+    // Add the 'active' class to the clicked button and the corresponding section
+    link.classList.add('active');
+    const targetPage = link.textContent.toLowerCase();
+    document.querySelector(`[data-page="${targetPage}"]`).classList.add('active');
   });
-}
+});
